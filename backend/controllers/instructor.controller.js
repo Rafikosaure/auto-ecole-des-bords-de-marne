c3f79fefc2f8c8c4e7408c58aaaf011a3b45ab1d
Julien Dorsan <juliendorsan@gmail.com>
2024-09-12 16:35:56 +0200

    Les erreurs des controllers sont maintenant traitées de manière centralisée. Ajout de la création d'un Admin par défaut pour la prmière connexion au site.

--- backend/controllers/instructor.controller.js
+++ backend/controllers/instructor.controller.js
@@ -1,5 +1,12 @@
 const { Instructor } = require("../models");
 
+const { errorHandler,
+    createError,
+    ErrorNotExist} = require('../middlewares/errorHandler.js');
+
+// error handling
+const context = "Instructor";
+
 const addInstructor = async (req, res, next) => {
     try {
         // SQL create query
@@ -8,8 +15,7 @@ const addInstructor = async (req, res, next) => {
           });
           res.status(201).json(`Instructor ${req.body.lastName} ${req.body.firstName} has been registered!`);
     } catch (error) {
-        console.log(error.message);
-        res.status(404).json("An error has occured")
+        return errorHandler(req, res, error, context);
     }
 }
 
@@ -18,10 +24,8 @@ const getAllInstructors = async (req, res, next) => {
         // SQL Select query to get all instructors
         const instructors = await Instructor.findAll();
         res.status(200).json(instructors);
-        console.table(instructors);
     } catch (error) {
-        console.log(error.message);
-        res.status(404).json("Could not get instructors", error.message);
+        return errorHandler(req, res, error, context);
     }
 }
 
@@ -30,11 +34,10 @@ const getInstructor = async (req, res, next) => {
         // SQL Select query to get one instructor by ID
         const instructor = await Instructor.findByPk(req.params.id);
         // error if no instructor found given the id
-        if(!instructor) return res.status(404).json("Instructor not found");
+        if(!instructor) throw createError(req, ErrorNotExist, context);
         res.status(200).json(instructor);
     } catch (error) {
-        console.log(error.message);
-        res.status(404).json("An error has occured", error.message);
+        return errorHandler(req, res, error, context);
     }
 }
 
@@ -43,13 +46,12 @@ const updateInstructor = async (req, res, next) => {
         // SQL Select query to get one instructor by ID
         const instructor = await Instructor.findByPk(req.params.id);
         // return an error if no instructor found
-        if (!instructor) return res.status(404).json("Instructor not found");
+        if (!instructor) throw createError(req, ErrorNotExist, context);
         // SQL Select query to update selected instructor with request's body
         await instructor.update(req.body)
         res.status(200).json({message: "instructor updated", instructor});
     } catch (error) {
-        console.log(error.message);
-        res.status(404).json("An error has occured", error.message);  
+        return errorHandler(req, res, error, context);
     }
 }
 
@@ -58,11 +60,10 @@ const deleteInstructor = async (req, res, next) => {
         // SQL Delete query to delete one instructor by ID
         const instructor = await Instructor.destroy({ where: { id: req.params.id } });
         // return error if instructor not found
-        if (!instructor) return res.status(404).json("Instructor not found");
+        if (!instructor) throw createError(req, ErrorNotExist, context);
         res.status(200).json({ message: "Instructor Deleted" });
   } catch (error) {
-      console.log(error.message);
-      res.status(404).json("An error has occured", error.message);      
+        return errorHandler(req, res, error, context);    
   }
 }
 
