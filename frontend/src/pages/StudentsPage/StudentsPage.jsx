8ce00006483721c15e2484a3ce814efa2cd49803
Marianna Demchenko <mdemko2012@gmail.comm>
2024-09-20 12:18:35 +0200

    une branche

--- frontend/src/pages/StudentsPage/StudentsPage.jsx
+++ frontend/src/pages/StudentsPage/StudentsPage.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Students from "../../components/Students/Students";
 
-
 const StudentsPage = () => {
   return (
     <>
       <Students />
-
     </>
   );
-}
+};
 
 export default StudentsPage;
