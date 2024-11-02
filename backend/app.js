a1c9bbc311ebc980a46a73bd75eb44e436a24080
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-11-02 14:46:33 +0100

    Ajout de la prise en charge des routes et fonctionnalités pour la gestion des emails et des pièces jointes aux emails, ainsi que des images diverses incluses dans les modèles EJS.

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,16 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const ENV = require("./config/env.js").ENV;
+const ENV = require("./config/env.js").ENV
+const { expressApp } = require("nodemailer-mail-tracking")
+const { mailTrackingOptions } = require("./sharedFunctions/mailTrackingOptions.js");
+const path = require('path');
 const studentRouter = require("./routes/student.router.js");
 const instructorRouter = require("./routes/instructor.router.js");
 const adminRouter = require("./routes/admin.router.js");
 const documentRouter = require("./routes/document.router.js");
 const remarkRouter = require("./routes/remark.router.js");
+const emailRouter = require("./routes/email.router.js")
 
 // app
 const app = express();
@@ -26,12 +30,19 @@ app.use(cors({
 // Preflight Requests
 app.options('*', cors());
 
+// STATIC FILES FOR STUDENT CONTRACT
+app.use('/contract-signatures', express.static(path.join(__dirname, './assets/contractImages')));
+
+// EMAIL TRACKING
+app.use(`/api/${ENV.EMAIL_TRACKING_ENDPOINT}`, expressApp(mailTrackingOptions));
+
 // URLS API PREFIX
 app.use("/api/student", studentRouter);
 app.use("/api/instructor", instructorRouter);
 app.use("/api/admin", adminRouter);
 app.use("/api/document", documentRouter);
 app.use("/api/remark", remarkRouter);
+app.use('/api/emails', emailRouter)
 
 // exports
 exports.app = app;
