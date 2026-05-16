e061b00c56b99394d186975a4f97df019b30528d
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2026-05-16 22:05:42 +0200

    Reprise des styles inline du composant PrintContractView.jsx

--- frontend/src/components/StudentContract/PrintContractView.jsx
+++ frontend/src/components/StudentContract/PrintContractView.jsx
@@ -6,10 +6,10 @@ import './PrintContractStyles.css'
 export default function PrintContractView({ setNumberOfComponent, student }) {
     return (
         <section className="contract-view-section mt-4 mb-3 container-md d-flex flex-column justify-content-between border border-lightgrey border-1 rounded">
-            <div style={{ marginLeft: 'auto', marginRight: 'auto', marginTop: '10px', marginBottom: '10px' }}>
+            <div className="mx-auto mt-2 mb-2">
                 <PrintContractButton setNumberOfComponent={setNumberOfComponent} student={student} /></div>
             <div className="contract-view-main d-flex flex-column">
-                <div className="d-flex justify-content-center align-items-center" style={{ width: "auto", height: "20px", fontSize: "12px",                 backgroundColor: "lightgreen" }}>Modifiez le document si nécessaire
+                <div className="d-flex justify-content-center align-items-center bg-success bg-opacity-25" style={{ height: "20px", fontSize: "12px" }}>Modifiez le document si nécessaire
                 </div>
                 <div className="viewer container-md d-flex justify-content-center align-items-center bg-secondary bg-opacity-25 mb-3">                    
                     <PrintContractViewerWindow setNumberOfComponent={setNumberOfComponent} student={student} />
